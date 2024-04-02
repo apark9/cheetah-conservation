1b1a94959df6e982758841f1498db97e191c6584
Avery Park <averypark@college.harvard.edu>
2024-04-02 01:46:39 -0400

    a bunch of disorganized notes

--- cluster.py
+++ cluster.py
@@ -2,33 +2,37 @@
 # CLUSTER ALGORITHM  # 
 # ////////////////// #
 
-
-# How to know if two clustesr are similar?
-# look at distance between clusters
-# most common distance EQU: Euclidian
-
-# How do we measure distance, as clusters may have multiple points?
-# Centroid distance: distance btw centroids
-# Complete linkage: distance btw two farthest points
-# Single linkages: distance btw two nearest points
-# Average linkages: distance btw point and all other points, done for all points, then averaged
-# Ward's linkage: distance that minimizes variance within cluster, maximizes variance between clusters
-
-# How can we graphically represent the history of making our clusters?
-# Dendrogram
-
-# AgglomerativeClustering object performs a hierarchical clustering using a bottom up approach
-
-
+'''
+HIGH-LEVEL NOTES
+'''
+# 1. initial clustering: split dataset into two clusters
+# 2. select parents: two most dissimilar datapoints (could do this through scipy.spatial.distance)
+# 3. generate children: merge parents and create children
+# 4. add children: add children to dataset
+# 5. repeat: repeat steps 2-4 until stopping criterion is met
+# 6. stopping criterion: number of clusters is equal to the number of clusters specified
 
 # IMPORTS
-from sklearn.cluster import AgglomerativeClustering
-from scipy.cluster.hierarchy import dendrogram
 import matplotlib.pyplot as plt
 
-# here is some text
-# MODEL 
-# X = data matrix with more than 2 variables
-model = AgglomerativeClustering(affinity='euclidean', linkage='ward')
-clusters = model.fit_predict(X)
+'''
+TO FIGURE OUT LATER
+'''
+# understand HiPart / DePDDP
+# have to figure out how to merge parents, how do we select two specific datapoints to merge?
 
+# DIVISIVE PACKAGE ATTEMPT
+from HiPart.clustering import DePDDP
+from sklearn.datasets import make_blobs
+
+# for later, HiPart allows for PCA / other dimensionality reduction steps
+# for now, we will just use the data as is
+
+X, y = make_blobs(n_samples=100, centers=6, n_features=2, random_state=42)
+clustered_class = DePDDP(max_clusters_number=6).fit_predict(X)
+
+plt.scatter([x[0] for x in X], [x[1] for x in X], c=clustered_class)
+plt.title('Divisive Clustering Results')
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+plt.show()
